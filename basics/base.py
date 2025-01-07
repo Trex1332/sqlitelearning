0f44e0635d37f86407e9a3a8b72f388d67928a4e
ThePrimeagen <samueljamieson02@gmail.com>
2025-01-07 18:14:55 +0000

    set up db stuff

--- basics/base.py
+++ basics/base.py
@@ -4,3 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 basedir =os.path.abspath(os.path.dirname(__file__))
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite') #sets up db locatin
+app.config['SQLALCHEMY_TRACK_MODIFICATONS']= False
+
+db = SQLAlchemy(app)
+
