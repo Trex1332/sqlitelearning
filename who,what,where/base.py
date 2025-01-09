0f2a478c8ec17eb632ee8609e8d4ef92e9c9881e
ThePrimeagen <samueljamieson02@gmail.com>
2025-01-09 18:01:42 +0000

    set out tabel

--- who,what,where/base.py
+++ who,what,where/base.py
@@ -14,4 +14,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATONS']= False
 db = SQLAlchemy(app)
 
 class whowhatwhere(db.Model):
-    
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Colum(db.Text)
+    game = db.Column(db.Text)
+    character = db.Column(db.Text)
+
+    def __init__(self,name,game,character):
+        self.name = name
+        self.game = game
+        self.character = character
+
+    def __repr__(self):
+        return f"Name is {self.name}, Their Favorite character is from {self.game}, the character is {self,character}"
